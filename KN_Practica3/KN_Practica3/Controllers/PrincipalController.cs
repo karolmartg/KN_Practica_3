c7d86a8eb69043d11630092a6a8480210185f2f9
JPMG2599 <99611344+JPMG2599@users.noreply.github.com>
2025-04-11 19:59:46 -0600

    Consulta
    Parte Consulta

--- KN_Practica3/KN_Practica3/Controllers/PrincipalController.cs
+++ KN_Practica3/KN_Practica3/Controllers/PrincipalController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KN_Practica3.BaseDatos;
 
 namespace KN_Practica3.Controllers
 {
     public class PrincipalController : Controller
     {
+        private PracticaS12Entities db = new PracticaS12Entities();
         // GET: Principal
         public ActionResult Consulta()
         {
-            return View();
+            var productos = db.SP_Consultar_Producto().ToList();
+
+
+            return View(productos);
         }
     }
 }
